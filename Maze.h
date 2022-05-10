5a5663aa873666f11d37aba1fc8ce83f2dd8d974
jason <Liujason2003@gmail.com>
2022-05-10 14:11:02 +0200

    ADT maze

--- Maze.h
+++ Maze.h
@@ -1,9 +1,13 @@
 #ifndef AUTOMATA_GROUP_PROJECT_2021_2022_MAZE_H
 #define AUTOMATA_GROUP_PROJECT_2021_2022_MAZE_H
+#include <vector>
 
+using namespace std;
+
+class Path;
 
 class Maze {
-
+    vector<Path*> maze;
 };
 
 
