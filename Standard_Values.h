e6fb02978ab3bba208987cc36a4084ffc88332c9
jason <Liujason2003@gmail.com>
2022-05-10 14:15:05 +0200

    Levels as standard values

--- Standard_Values.h
+++ Standard_Values.h
@@ -1,6 +1,9 @@
 #ifndef AUTOMATA_GROUP_PROJECT_2021_2022_STANDARD_VALUES_H
 #define AUTOMATA_GROUP_PROJECT_2021_2022_STANDARD_VALUES_H
 
-
+#define LEV1                    "Level1.txt"
+#define LEV2                    "Level2.txt"
+#define LEV3                    "Level3.txt"
+#define LEV4                    "Level4.txt"
 
 #endif //AUTOMATA_GROUP_PROJECT_2021_2022_STANDARD_VALUES_H
