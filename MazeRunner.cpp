02b813fdb6a6b7e41bd71027bcfa8e1ac4755396
OrfeoTerkuci <terkuciorfeo@gmail.com>
2022-05-17 16:10:15 +0200

    Latest : maze generation

--- MazeRunner.cpp
+++ MazeRunner.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include "Maze.h"
 
 using namespace std;
 
 int main(int argc, char** argv){
+    Maze* newMaze = new Maze("Level1.json");
+    newMaze->loadGame("../TXT-Files/Level1.txt");
     cout << "Ha you ran this bullshit" << endl;
 }
