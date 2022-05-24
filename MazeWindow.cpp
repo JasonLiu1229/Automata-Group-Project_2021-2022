6c38732666b14a88c7750a0f06e4a51f16b7840f
Slendy <siebe.wijnants@hotmail.com>
2022-05-24 15:43:12 +0200

    window try

--- MazeWindow.cpp
+++ MazeWindow.cpp
@@ -5,10 +5,13 @@
 #include "MazeWindow.h"
 #include <QtUiTools/QUiLoader>
 
+
 QWidget *MazeWindow::loadUiFile(QWidget *parent) {
+    /*
     QFile file("../../QT_Utils/MazeWindowLayout.ui");
     file.open(QIODevice::ReadOnly);
 
     QUiLoader loader;
     return loader.load(&file, parent);
+    */
 }
