58894979740aa2fa53a105107872858eae6ec282
Izaseth <101905898+Izaseth@users.noreply.github.com>
2022-05-31 17:42:46 +0200

    Add files via upload
    dfa naar regex

---
+++ Algorithms/X/untitled25/cmake-build-debug/CMakeFiles/untitled25.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled25.dir/DFA.cpp.o"
+  "CMakeFiles/untitled25.dir/NFA.cpp.o"
+  "CMakeFiles/untitled25.dir/main.cpp.o"
+  "untitled25"
+  "untitled25.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled25.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
