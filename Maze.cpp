f8bc83d3a74f50fbc5feebcd5f0c9f2ca3136da8
OrfeoTerkuci <terkuciorfeo@gmail.com>
2022-06-03 16:16:00 +0200

    Latest

--- Maze.cpp
+++ Maze.cpp
@@ -418,7 +418,46 @@ void Maze::simulateMove(movement m) {
 }
 
 // algorithms
+Player* Maze::getPlayer() {
+    return player;
+}
+Collectable_DFA* Maze::getDFAkeys() {
+    return collectedKeys;
+}
 
+bool Maze::checkmaze(Path* richting){
+    if(richting->isEnemy()){
+        return true;
+    }
+    return false;
+}
+// algorithms
+
+void Maze::EnemyMovement() {
+    for (auto enemy: enemies) {
+        movement move = enemy->moveSmartV2();
+        if (move == UP and !enemy->GetCurrentTile()->getUp()->isEnemy()) {
+            enemy->getCurrentTile()->setEnemy(false);
+            enemy->setCurrentTile(enemy->GetCurrentTile()->getUp());
+            enemy->getCurrentTile()->setEnemy(true);
+        }else if (move == DOWN and !enemy->GetCurrentTile()->getDown()->isEnemy()) {
+            enemy->getCurrentTile()->setEnemy(false);
+            enemy->setCurrentTile(enemy->GetCurrentTile()->getDown());
+            enemy->getCurrentTile()->setEnemy(true);
+        } else if (move == RIGHT and !enemy->GetCurrentTile()->getRight()->isEnemy()) {
+            enemy->getCurrentTile()->setEnemy(false);
+            enemy->setCurrentTile(enemy->GetCurrentTile()->getRight());
+            enemy->getCurrentTile()->setEnemy(true);
+        } else if (move == LEFT and !enemy->GetCurrentTile()->getLeft()->isEnemy()) {
+            enemy->getCurrentTile()->setEnemy(false);
+            enemy->setCurrentTile(enemy->GetCurrentTile()->getLeft());
+            enemy->getCurrentTile()->setEnemy(true);
+        }
+        if(enemy->getCurrentTile() == player->GetCurrentTile()){
+            player->playerdied();
+        }
+    }
+}
 /*TFA minimize*/
 
 void Maze::recursionMinimize(Maze *&maze, map<pair<Path*, Path*>, bool> &Table, set<pair<Path*, Path*>> &markedStates) {
